54822eaea0fceb6b086103c1446c253395907b17
Olga Shcherbatiuk <nika32.n3@gmail.com>
2026-09-15 17:01:47 +0300

    fix: add catch

--- src/api/goods.ts
+++ src/api/goods.ts
@@ -14,11 +14,19 @@ export function getAll(): Promise<Good[]> {
 }
 
 export const get5First = (): Promise<Good[]> => {
-  return getAll().then(goods =>
-    [...goods].sort((a, b) => a.name.localeCompare(b.name)).slice(0, 5),
-  ); // sort and get the first 5
+  return getAll()
+    .then(goods =>
+      [...goods].sort((a, b) => a.name.localeCompare(b.name)).slice(0, 5),
+    )
+    .catch(error => {
+      throw error('Error in get5First');
+    }); // sort and get the first 5
 };
 
 export const getRedGoods = (): Promise<Good[]> => {
-  return getAll().then(goods => goods.filter(good => good.color === 'red')); // get only red
+  return getAll()
+    .then(goods => goods.filter(good => good.color === 'red'))
+    .catch(error => {
+      throw error('Error in getRedGoods');
+    }); // get only red
 };
